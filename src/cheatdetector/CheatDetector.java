1f356dc799a6f9372c02edcb9a477b49cbf152cd
Philip Rodriguez <philiprodriguez@knights.ucf.edu>
2017-08-09 12:52:20 -0400

    Cleaned up imports

--- src/cheatdetector/CheatDetector.java
+++ src/cheatdetector/CheatDetector.java
@@ -1,11 +1,7 @@
 package cheatdetector;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.nio.file.Paths;
 import java.util.ArrayList;
